727109ac095cc98d685febdfec4f6bfb38d1e54d
Aryav <tiwarikaustav@gmail.com>
2020-10-06 00:40:19 +0530

    Contacts can be added

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Address Book!");
+            Console.WriteLine("Welcome to Address Book Problem!");
             AddressBook addressBook = new AddressBook();
             addressBook.AddContact();
 
