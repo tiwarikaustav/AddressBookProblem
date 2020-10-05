00f0f197ee394368666a81f45ce78665acb9ad76
Aryav <tiwarikaustav@gmail.com>
2020-10-05 20:16:33 +0530

    Ability to add multiple contacts added

--- AddressBook.cs
+++ AddressBook.cs
@@ -11,26 +11,38 @@ namespace AddressBookSystem
 
         public void AddContact()
         {
-            ContactDetails contact = new ContactDetails();
-            if(contact.firstName == "" || contact.lastName == "" || contact.address == "" || contact.city == "" || contact.state == "" || contact.zip == "" || contact.phoneNum == "" || contact.eMail == "")
-            {
-                //Just to mark that this operation was unsuccessful
-                nLog.LogDebug("Operation Unsuccessful: AddContact()");
-                //Just to make note that whatever values entered should be greater than zero
-                nLog.LogError("Values entered by user cannot be empty string");
-                //Just to warn that values should be non empty
-                nLog.LogWarn("Entry should not be empty");
-            }
-            
-            contactList.Add(contact);
-            Console.WriteLine("Contact created successfully with following details: ");
-            Console.WriteLine("First Name: "+ contact.firstName + " Last Name: " + contact.lastName);
-            Console.WriteLine("Address: " + contact.address + " \nCity: " + contact.city + " \nState: " + contact.state);
-            Console.WriteLine("Zip Code: " + contact.zip + " \nPhone Number: " + contact.phoneNum + " \nEMail ID: " + contact.eMail);
+            bool flag = true;
+            while (flag) 
+            { 
+                ContactDetails contact = new ContactDetails();
+                if (contact.firstName == "" || contact.lastName == "" || contact.address == "" || contact.city == "" || contact.state == "" || contact.zip == "" || contact.phoneNum == "" || contact.eMail == "")
+                {
+                    //Just to mark that this operation was unsuccessful
+                    nLog.LogDebug("Operation Unsuccessful: AddContact()");
+                    //Just to make note that whatever values entered should be greater than zero
+                    nLog.LogError("Values entered by user cannot be empty string");
+                    //Just to warn that values should be non empty
+                    nLog.LogWarn("Entry should not be empty");
+                }
 
-            nLog.LogDebug("Log Successful: AddContact()");
-            //Just to mark a flag that this operation was successful
-            nLog.LogInfo("Contact Created with first name: " + contact.firstName + " last name: " + contact.lastName);
+                contactList.Add(contact);
+                Console.WriteLine("Contact created successfully with following details: ");
+                Console.WriteLine("First Name: " + contact.firstName + " Last Name: " + contact.lastName);
+                Console.WriteLine("Address: " + contact.address + " \nCity: " + contact.city + " \nState: " + contact.state);
+                Console.WriteLine("Zip Code: " + contact.zip + " \nPhone Number: " + contact.phoneNum + " \nEMail ID: " + contact.eMail);
+
+                nLog.LogDebug("Log Successful: AddContact()");
+                //Just to mark a flag that this operation was successful
+                nLog.LogInfo("Contact Created with first name: " + contact.firstName + " last name: " + contact.lastName);
+
+                //If multiple contacts are to be added
+                Console.WriteLine("Do you want to add another contact? Press Y/N");
+                string option = Console.ReadLine();
+                if (!(option != "Y" ^ option != "y"))
+                {
+                    flag = false;
+                }
+            }
         }
     }
 }
